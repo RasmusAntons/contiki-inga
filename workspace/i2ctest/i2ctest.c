2eeb36b636390a35d9046c58b9dfb419c440f2b6
= <=>
2016-05-23 12:51:42 +0200

    Motorsteuerung Beispiel

---
+++ workspace/i2ctest/i2ctest.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2006, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         A very simple Contiki application showing how Contiki programs look
+ * \author
+ *         Adam Dunkels <adam@sics.se>
+ */
+
+#include "contiki.h"
+#include <stdio.h> /* For printf() */
+#include "dev/i2c.h"
+#include "dev/button-sensor.h"
+
+#define I2C_ADDR 0x66
+/*---------------------------------------------------------------------------*/
+PROCESS(hello_world_process, "Hello world process");
+AUTOSTART_PROCESSES(&hello_world_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(hello_world_process, ev, data)
+{
+	PROCESS_BEGIN();
+
+	printf("Hello, world\n");
+	
+	SENSORS_ACTIVATE(button_sensor);
+	
+	i2c_init();
+	
+	static int8_t left = 0, right = 20;
+	
+	while(1)
+	{
+		printf("setting left to %d and right to %d\n", left, right);
+		
+		int s1 = i2c_start(I2C_ADDR);
+		int w11 = i2c_write('l');
+		int w12 = i2c_write(left);
+		i2c_stop();
+		
+		int s2 = i2c_start(I2C_ADDR);
+		int w21 = i2c_write('r');
+		int w22 = i2c_write(right);
+		i2c_stop();
+		
+		printf("s1 = %d, w11 = %d, w12 = %d, s2 = %d, w21 = %d, w22 = %d\n\n", s1, w11, w12, s2, w21, w22);
+		
+		PROCESS_YIELD();
+		PROCESS_YIELD();
+		
+		right = (right == 20) ? -20 : 20;
+		
+	}
+	
+	PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
